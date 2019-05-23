526499187319c179911c06bd64d80bbebf9bde47
Daniel Kelly <djk8552@gmail.com>
2019-05-23 10:54:29 -0400

    Orgs route/template

---
+++ app/routes/organizations_route.js
@@ -0,0 +1,19 @@
+/**
+  * # Organizations Route
+  * @description Route for dealing with all organization objects
+  * @todo This is really the users_index route and should be named as such by convention
+  * @author Amir Tahvildaran <amir@mathforum.org>
+  * @since 1.0.0
+  */
+ Encompass.OrganizationsRoute = Encompass.AuthenticatedRoute.extend({
+  model: function () {
+    return Ember.RSVP.hash({
+      organizations: this.get('store').findAll('organization'),
+    });
+  },
+
+  renderTemplate: function(){
+    this.render('organizations/organizations');
+  }
+
+});
