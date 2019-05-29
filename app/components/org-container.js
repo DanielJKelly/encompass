7fac5db9b05bf7628c292e17f7dc84c0eaaa26cc
Daniel Kelly <djk8552@gmail.com>
2019-05-29 11:00:29 -0400

    Identify active org in list

--- app/components/org-container.js
+++ app/components/org-container.js
@@ -1,3 +1,11 @@
 Encompass.OrgContainerComponent = Ember.Component.extend(Encompass.CurrentUserMixin, {
 elementId: 'org-container',
+
+activeOrg: null,
+
+actions: {
+  setActiveOrg(org) {
+    this.set('activeOrg', org);
+  }
+}
 });
--- app/components/org-list-item.js
+++ app/components/org-list-item.js
@@ -1,3 +1,6 @@
 Encompass.OrgListItemComponent = Ember.Component.extend(Encompass.CurrentUserMixin, {
   classNames: ['org-list-item', 'side-list-item'],
+  classNameBindings: ['isActive:is-active'],
+
+  isActive: Ember.computed.equal('activeOrg.id', this.getComputedStyle('org.id')),
 });
