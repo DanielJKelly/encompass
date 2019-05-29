9adb2b9d5fa0f9e97a110380e87d9bdd128a4e04
Daniel Kelly <djk8552@gmail.com>
2019-05-29 11:01:00 -0400

    Update org info page

--- app/components/org-info.js
+++ app/components/org-info.js
@@ -1,100 +1,166 @@
-Encompass.OrgInfoComponent = Ember.Component.extend(Encompass.CurrentUserMixin, {
-elementId: 'org-info',
-utils: Ember.inject.service('utility-methods'),
-alert: Ember.inject.service('sweet-alert'),
+Encompass.OrgInfoComponent = Ember.Component.extend(
+  Encompass.CurrentUserMixin,
+  {
+    elementId: 'org-info',
+    utils: Ember.inject.service('utility-methods'),
+    alert: Ember.inject.service('sweet-alert'),
 
-didReceiveAttrs() {
-  this.set('allUsers', this.get('store').peekAll('user')).rejectBy('isTrashed');
+    didReceiveAttrs() {
+      this.set('allUsers', this.get('store').peekAll('user')).rejectBy(
+        'isTrashed'
+      );
 
-  this.set('isEditing', false);
-
-  this.set('cachedUsers', this.get('store').peekAll('user'));
-
-  this._super(...arguments);
-},
-
-pdAdmins: function() {
-  return this.get('model.members').filterBy('accountType', 'P');
-}.property('model.members.@each.accountType'),
-
-canEdit: function() {
-  if (this.get('currentUser.accountType') === 'A') {
-    return true;
-  }
-
-  // if non admin can edit org info if pdadmin
-  if (this.get('currentUser.accountType') !== 'P') {
-    return false;
-  }
-
-  let userOrgId = this.get('utils').getBelongsToId(this.get('currentUser'), 'organization');
-
-  return userOrgId === this.get('model.id');
-
-}.property('currentUser.accountType', 'currentUser.organization', 'model'),
-
-existingMemberIdHash: function() {
-  let members = this.get('model.members') || [];
-  let hash = {};
-
-  members.forEach((member) => {
-    hash[member.get('id')] = true;
-  });
-  return hash;
-
-}.property('model.members.[]'),
-
-initialMemberOptions: function() {
-  let cached = this.get('cachedUsers') || [];
-  let nonMemberCachedUsers = cached.reject((user) => {
-    return this.get('existingMemberIdHash.' + user.get('id'));
-  });
-  return nonMemberCachedUsers.map((user) => {
-    return {
-      id: user.get('id'),
-      username: user.get('username')
-    };
-  });
-
-}.property('cachedUsers.[]', 'model.members.[]'),
-
-actions: {
-  beginEditing() {
-    this.set('updatedOrgName', this.get('model.name'));
-    this.set('membersToAdd', []);
-    this.set('pdAdminsToAdd', []);
-
-    this.set('isEditing', true);
-  },
-
-  stopEditing() {
-    // TODO: confirm cancel if changes made?
-    this.set('isEditing', false);
-  },
-
-  saveEdits() {
-    let newName = this.get('updatedOrgName');
-
-    let trimmedName = typeof newName === 'string' ? newName.trim() : '';
-
-    let didNameChange = trimmedName.length > 0 && trimmedName !== this.get('model.name');
-
-    if (!didNameChange) {
+        this.parentView.send('setActiveOrg', this.get('model'));
       this.set('isEditing', false);
-      return this.get('alert').showToast('info', 'No changes to save', 'bottom-end', 3000, false, null);
-    }
 
-  },
-  updateMembersToAdd(userId, $item) {
-    if (!userId) {
-      return;
-    }
-    let user = this.get('store').peekRecord('user', userId);
+      this.set('cachedUsers', this.get('store').peekAll('user'));
 
-    if (!user) {
-      return;
-    }
+      this._super(...arguments);
+    },
 
-  },
-}
-});
+    areMembersToAdd: Ember.computed.gt('membersToAdd.length', 0),
+
+    pdAdmins: function() {
+      return this.get('model.members').filterBy('accountType', 'P');
+    }.property('model.members.@each.accountType'),
+
+    canEdit: function() {
+      if (this.get('currentUser.accountType') === 'A') {
+        return true;
+      }
+
+      // if non admin can edit org info if pdadmin
+      if (this.get('currentUser.accountType') !== 'P') {
+        return false;
+      }
+
+      let userOrgId = this.get('utils').getBelongsToId(
+        this.get('currentUser'),
+        'organization'
+      );
+
+      return userOrgId === this.get('model.id');
+    }.property('currentUser.accountType', 'currentUser.organization', 'model'),
+
+    existingMemberIdHash: function() {
+      let members = this.get('model.members') || [];
+      let hash = {};
+
+      members.forEach(member => {
+        hash[member.get('id')] = true;
+      });
+      return hash;
+    }.property('model.members.[]'),
+
+    initialMemberOptions: function() {
+      let cached = this.get('cachedUsers') || [];
+      let nonMemberCachedUsers = cached.reject(user => {
+        return this.get('existingMemberIdHash.' + user.get('id'));
+      });
+      return nonMemberCachedUsers.map(user => {
+        return {
+          id: user.get('id'),
+          username: user.get('username')
+        };
+      });
+    }.property('cachedUsers.[]', 'model.members.[]'),
+
+    actions: {
+      beginEditing() {
+        this.set('updatedOrgName', this.get('model.name'));
+        this.set('membersToAdd', []);
+        this.set('pdAdminsToAdd', []);
+
+        this.set('isEditing', true);
+      },
+
+      stopEditing() {
+        // TODO: confirm cancel if changes made?
+        this.set('isEditing', false);
+      },
+
+      saveEdits() {
+        let newName = this.get('updatedOrgName');
+
+        let trimmedName = typeof newName === 'string' ? newName.trim() : '';
+
+        let didNameChange =
+          trimmedName.length > 0 && trimmedName !== this.get('model.name');
+
+        if (!didNameChange) {
+          this.set('isEditing', false);
+          return this.get('alert').showToast(
+            'info',
+            'No changes to save',
+            'bottom-end',
+            3000,
+            false,
+            null
+          );
+        }
+      },
+      updateMembersToAdd(userId, $item) {
+        if (!userId) {
+          return;
+        }
+
+        let user = this.get('store').peekRecord('user', userId);
+
+        if (!user) {
+          return;
+        }
+
+        if ($item === null) {
+          // removal
+          return this.get('membersToAdd').removeObject(user);
+        }
+
+        return this.get('membersToAdd').addObject(user);
+      },
+
+      // removeMember(member) {
+      //   if (!member) {
+      //     return;
+      //   }
+
+      //   let username = member.get('username');
+      //   let orgName = this.get('model.name');
+
+      //   let warningText = `Are you sure you want to remove ${username} from ${orgName}?`;
+      //   let confirmText = 'Yes, remove.';
+
+      //   let successText = 'Member removed';
+
+      //   let undoBtnText = 'Undo';
+      //   let undoSuccessText = 'Member re-added';
+
+      //   return this.get('alert').showModal('warning', warningText, null, confirmText)
+      //     .then((result) => {
+      //       if (result.value) {
+      //         // remove user from org members
+      //         this.get('model').removeObject(member);
+
+      //         // update user's org
+
+      //         member.set('organization', null);
+      //         return Ember.RSVP.hash({
+      //           org: this.get('model').save(),
+      //           member: member.save(),
+      //         })
+      //         .then(())
+      //         return this.get('alert').showToast('success', successText, 'bottem-end', 3000, true, undoBtnText)
+      //           .then((result) => {
+      //             if (result.value) {
+      //               return this.get('alert').showToast('success', undoSuccessText, 'bottem-end', 3000, false, null);
+      //             }
+      //           });
+      //       }
+      //     })
+      //     .catch((err) => {
+      //       this.handleErrors(err, 'modelUpdateErrors', this.get('model'));
+      //     });
+
+      // }
+    }
+  }
+);
