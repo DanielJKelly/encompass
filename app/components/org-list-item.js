ba54b5fb73f9a9becd3b77e71f3e4235443cf3a6
Daniel Kelly <djk8552@gmail.com>
2019-05-30 13:24:08 -0400

    Use strikethrough class for trashed org names

--- app/components/org-list-item.js
+++ app/components/org-list-item.js
@@ -1,6 +1,6 @@
 Encompass.OrgListItemComponent = Ember.Component.extend(Encompass.CurrentUserMixin, {
   classNames: ['org-list-item', 'side-list-item'],
-  classNameBindings: ['isActive:is-active'],
+  classNameBindings: ['isActive:is-active', 'org.isTrashed:strikethrough'],
 
   isActive: function() {
     return this.get('activeOrg.id') === this.get('org.id');
