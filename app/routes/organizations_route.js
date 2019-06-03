245e00fcccbcf8c09f9b0dc0a90e9c635e3fb2e0
Daniel Kelly <djk8552@gmail.com>
2019-06-03 17:45:57 -0400

    Cleanup

--- app/routes/organizations_route.js
+++ app/routes/organizations_route.js
@@ -1,30 +1,32 @@
 /**
-  * # Organizations Route
-  * @description Route for dealing with all organization objects
-  * @todo This is really the users_index route and should be named as such by convention
-  * @author Amir Tahvildaran <amir@mathforum.org>
-  * @since 1.0.0
-  */
- Encompass.OrganizationsRoute = Encompass.AuthenticatedRoute.extend({
-  model: function () {
-    // return this.get('store').findAll('organization');
-    return this.get('store').query('organization', {
-      filterBy: {
-        includeTrashed: 0,
-      },
-      sortBy: {
-        members: -1,
-      }
-    })
-    .then((results) => {
-      return {
-        orgs: results.toArray(),
-        meta: results.get('meta')
-      };
-    });
+ * # Organizations Route
+ * @description Route for dealing with all organization objects
+ * @todo This is really the users_index route and should be named as such by convention
+ * @author Amir Tahvildaran <amir@mathforum.org>
+ * @since 1.0.0
+ */
+Encompass.OrganizationsRoute = Encompass.AuthenticatedRoute.extend({
+  model: function() {
+    let filterBy = {
+      includeTrashed: 0
+    };
+
+    let sortBy = 'name';
+    let sortDirection = 1;
+
+    let queryHash = { filterBy, sortBy, sortDirection };
+
+    return this.get('store')
+      .query('organization', queryHash)
+      .then(results => {
+        return {
+          orgs: results.toArray(),
+          meta: results.get('meta')
+        };
+      });
   },
 
-  renderTemplate: function(){
+  renderTemplate: function() {
     this.render('organizations/organizations');
   },
   actions: {
@@ -32,8 +34,7 @@
       this.transitionTo('organizations.home');
     },
     toOrg(orgId) {
-     this.transitionTo('organization', orgId);
+      this.transitionTo('organization', orgId);
     }
   }
-
 });
